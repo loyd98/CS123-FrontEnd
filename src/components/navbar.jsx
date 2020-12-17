f67dadcef117f5e02afdedf49063dde1258953b3
John Loyd de Troz <john.detroz@obf.ateneo.edu>
2020-12-17 20:24:34 +0800

    Add changes to Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
+import "./navbar.css";
 
 class Navbar extends Component {
   render() {
-    return <h1>Navbar</h1>;
+    return (
+      <nav>
+        <div className="logo">
+          <h1>Food</h1>
+        </div>
+        <div className="menu">
+          <ul className="menu-group">
+            <li>
+              <a href="">Order</a>
+            </li>
+            <li>
+              <a href="">Cart</a>
+            </li>
+            <li>
+              <a href="">Profile</a>
+            </li>
+            <li>
+              <a href="">Log in</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
